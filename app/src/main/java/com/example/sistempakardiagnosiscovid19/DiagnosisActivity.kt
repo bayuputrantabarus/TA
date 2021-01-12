cec5ddfe0fc98014e2d42d3708a3f3e79b194bb3
bayuputrantabarus <77150906+bayuputrantabarus@users.noreply.github.com>
2021-01-12 22:38:06 +0700

    Add files via upload

---
+++ app/src/main/java/com/example/sistempakardiagnosiscovid19/DiagnosisActivity.kt
@@ -0,0 +1,11 @@
+package com.example.sistempakardiagnosiscovid19
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DiagnosisActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_diagnosis)
+    }
+}
